b5cffdd0d08e86c4fa6c0953a2f232e0737b9409
JoséSanz <josesanzmartin@hotmail.es>
2023-04-15 18:24:21 +0200

    services with CRUD added

--- src/app/features/services/student.service.ts
+++ src/app/features/services/student.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class StudentService {
+  private apiUrl = "http://localhost:3000";
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getStudents(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/students`);
+  }
+
+  createStudent(student: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/students`, student);
+  }
+
+  updateStudent(student: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/students/${student.id}`, student);
+  }
+
+  deleteStudent(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/students/${id}`);
+  }
 }
