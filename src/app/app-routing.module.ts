5d609cd4587097da16f310fc48c5c9f67c3fbea0
JoséSanz <josesanzmartin@hotmail.es>
2023-04-15 18:48:11 +0200

    added students component functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from "@angular/router";
 import { SignupComponent } from "./features/pages/signup/signup.component";
 import { LoginComponent } from "./features/pages/login/login.component";
 import { HomeComponent } from "./features/pages/home/home.component";
+import { StudentsComponent } from "./features/pages/students/students.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "login", pathMatch: "full" },
   { path: "login", component: LoginComponent },
   { path: "signup", component: SignupComponent },
   { path: "home", component: HomeComponent },
+  { path: "students", component: StudentsComponent },
 ];
 
 @NgModule({
